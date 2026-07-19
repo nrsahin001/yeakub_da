5742be50df7b37c797373b92060173e86d9ad916
nrsahin001 <sahin629785@gmail.com>
2026-07-19 21:40:44 +0530

    Update and rename config.js to js/config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js
 // IMPORTANT: Change this to your actual PythonAnywhere backend URL
 // e.g. "https://yourusername.pythonanywhere.com"
-const API_BASE_URL = "https://yourusername.pythonanywhere.com";
+const API_BASE_URL = "https://app.najibur.me";
